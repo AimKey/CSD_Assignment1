a58dd8d683cc2b6b2e172c42ea1683ee90025fee
MinhKiet <phamminhkiet24@gmail.com>
2024-02-27 08:54:30 +0700

    feat: fix bug thanh cong

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
 
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Heap planets = new Heap(8);
+        planets.insert(new Planet("Mercury", 0.383, 0.06, 0.39, 0.24));
+        planets.insert(new Planet("Venus", 0.949, 0.81, 0.72, 0.62));
+        planets.insert(new Planet("Earth", 1.000, 1.00, 1.00, 1.00));
+        planets.insert(new Planet("Mars", 0.532, 0.11, 1.52, 1.88));
+        planets.insert(new Planet("Jupiter", 11.209, 317.83, 5.20, 11.86));
+        planets.insert(new Planet("Saturn", 9.449, 95.16, 9.54, 29.45));
+        planets.insert(new Planet("Uranus", 4.007, 14.54, 19.19, 84.02));
+        planets.insert(new Planet("Neptune", 3.883, 17.15, 30.07, 164.79));
+        planets.traverse();
     }
 
 }
@@ -9,15 +18,9 @@ class Main {
 class Heap {
     private Planet[] planets;
     private int size = 0;
-    private int maxSize = 0;
-
-    // Arr[(i-1)/2] Returns the parent node.
-    // Arr[(2*i)+1] Returns the left child node.
-    // Arr[(2*i)+2] Returns the right child node.
 
     public Heap(int n) {
         planets = new Planet[n];
-        maxSize = n;
     }
 
     // Since this is int, which means that the number is already floor-ed
@@ -51,18 +54,14 @@ class Heap {
         size++;
     }
 
-    public void heapify() {
-        int i, s, f;
-        int x;
-        for (i = 1; i < planets.length; i++) {
-            x = a[i];
-            s = i; // s is a son, f=(s-1)/2 is father
-            while (s > 0 && x > a[(s - 1) / 2]) {
-                a[s] = a[(s - 1) / 2];
-                s = (s - 1) / 2;
-            }
-            ;
-            a[s] = x;
+    public void traverse() {
+        System.out.println("size: " + size);
+        for (int i = 0; i < planets.length / 2; i++) {
+            System.out.printf("Parrent:%10s%s\n", " ", planets[i]);
+            if (leftChild(i) < size)
+                System.out.printf("Left Child:%7s%s\n", " ", planets[leftChild(i)]);
+            if (rightChild(i) < size)
+                System.out.printf("Right child:%6s%s\n", " ", planets[rightChild(i)]);
         }
     }
 }
@@ -78,16 +77,22 @@ class Planet implements Comparable<Planet> {
         this.semimajorAxis = semimajorAxis;
         this.orbitalPeriod = orbitalPeriod;
     }
-
-    @Override
-    public String toString() {
-        return "Name: " + name + ", equatorialDiameter: " + equatorialDiameter + ", mass: " + mass
-                + ", semimajorAxis: " + semimajorAxis + ", orbitalPeriod: " + orbitalPeriod;
-    }
+    // TODO: Use this instead
+    // @Override
+    // public String toString() {
+    // return "Name: " + name + ", equatorialDiameter: " + equatorialDiameter + ",
+    // mass: " + mass
+    // + ", semimajorAxis: " + semimajorAxis + ", orbitalPeriod: " + orbitalPeriod;
+    // }
 
     @Override
     public int compareTo(Planet o) {
-        return (int) (this.mass - o.mass);
+        return (int) Math.ceil(this.mass - o.mass);
+    }
+
+    @Override
+    public String toString() {
+        return "Planet name: " + name + ", mass: " + mass;
     }
 
 }
