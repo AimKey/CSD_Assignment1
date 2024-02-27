c01c8bf86b0588e9108d96d1c5722b91ead4b928
MinhKiet <phamminhkiet24@gmail.com>
2024-02-27 08:16:43 +0700

    model

--- Main.java
+++ Main.java
@@ -1,6 +1,27 @@
 
 class Main {
     public static void main(String[] args) {
-        
+        System.out.println("Hello World");
     }
+
+}
+
+class Planet {
+    String name;
+    double equatorialDiameter, mass, semimajorAxis, orbitalPeriod;
+
+    public Planet(String name, double equatorialDiameter, double mass, double semimajorAxis, double orbitalPeriod) {
+        this.name = name;
+        this.equatorialDiameter = equatorialDiameter;
+        this.mass = mass;
+        this.semimajorAxis = semimajorAxis;
+        this.orbitalPeriod = orbitalPeriod;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + name + ", equatorialDiameter: " + equatorialDiameter + ", mass: " + mass
+                + ", semimajorAxis: " + semimajorAxis + ", orbitalPeriod: " + orbitalPeriod;
+    }
+
 }
